288d679468bb4c200402e1c31aeaf4e366045cb0
h1un <h1unha@naver.com>
2022-03-01 05:45:12 +0900

    feat : 키워드 등록 api

--- src/main/java/com/example/news/controller/NewsController.java
+++ src/main/java/com/example/news/controller/NewsController.java
@@ -5,15 +5,13 @@ import com.example.news.entity.News;
 import com.example.news.repository.KeywordRepository;
 import com.example.news.repository.NewsRepository;
 import com.example.news.site.naver.ApiExamSearch;
-import com.example.news.site.naver.NaverNewsVo;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -28,8 +26,8 @@ public class NewsController {
     ModelAndView modelAndView = new ModelAndView();
     ObjectMapper objectMapper = new ObjectMapper();
 
-    @GetMapping("/test")
-    public ModelAndView jointestHtml() {
+    @GetMapping("/news")
+    public ModelAndView nesw() {
 
         List<Keyword> keywords = keywordRepository.findAll();
         modelAndView.addObject("keywords", keywords);
@@ -38,32 +36,16 @@ public class NewsController {
         return modelAndView;
     }
 
-    @GetMapping("/keyword")
-    public List<Keyword> findKeyword() {
-
-        keywordRepository.findAll().stream().forEach(System.out::println);
-        return keywordRepository.findAll();
-    }
-
     @GetMapping("/news/collection")
     public ResponseEntity<?> newsCollection() throws JsonProcessingException {
 
 //        keywordRepository.findAll().stream().forEach(keyword -> {
-        String json = apiExamSearch.search("컨셉");
 
-//                NaverNewsVo naverNewsVo = objectMapper.readValue(json, NaverNewsVo.class);
+        String json = apiExamSearch.search("컨셉");
         JsonNode jsonNode = objectMapper.readTree(json);
 
-/*
-        News news = News.builder()
-//                            .keyword(keyword)
-                .link(items.getOriginallink())
-                .title(items.getTitle())
-                .description(items.getDescription())
-                .build();
-*/
 
-        for(JsonNode items : jsonNode.path("items")) {
+        for (JsonNode items : jsonNode.path("items")) {
             News news = News.builder()
 //                            .keyword(keyword)
                     .link(items.path("link").asText())
@@ -78,5 +60,14 @@ public class NewsController {
 
         return new ResponseEntity<String>("ok", HttpStatus.OK);
     }
+    @ResponseBody
+    @PostMapping("/keyword")
+    public ResponseEntity<?> keyword(@RequestParam String keyword){
+
+        keywordRepository.save(Keyword.builder().keyword(keyword).build());
+
+        return new ResponseEntity<String>("ok", HttpStatus.OK);
+    }
+
 
 }
