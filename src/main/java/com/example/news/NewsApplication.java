93170ea53957e0f5073f06e5529d38f1107578f9
h1un <h1unha@naver.com>
2022-03-04 20:18:02 +0900

    🧹

--- src/main/java/com/example/news/NewsApplication.java
+++ src/main/java/com/example/news/NewsApplication.java
@@ -8,21 +8,13 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 
 
-@RequiredArgsConstructor
 @SpringBootApplication
 public class NewsApplication {
-
-    public final KeywordService keywordService;
 
     public static void main(String[] args) {
         SpringApplication.run(NewsApplication.class, args);
 
     }
 
-    @EventListener(ApplicationReadyEvent.class)
-    void keywordCollection() {
-        keywordService.insertKeywordFirst("선거");
-        keywordService.insertKeywordFirst("코로나");
-    }
 
 }
