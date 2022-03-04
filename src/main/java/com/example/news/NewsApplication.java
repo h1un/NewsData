ada81a62f2af62085fb9880ce247c17e3295b279
h1un <h1unha@naver.com>
2022-03-04 20:54:01 +0900

    🧹

--- src/main/java/com/example/news/NewsApplication.java
+++ src/main/java/com/example/news/NewsApplication.java
@@ -1,11 +1,7 @@
 package com.example.news;
 
-import com.example.news.service.KeywordService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 
 
 @SpringBootApplication
