1cc8b0e41ebce484ff4c63cebfae4777c87368c5
h1un <h1unha@naver.com>
2022-03-06 17:08:38 +0900

    fix : 스케줄러 수정

--- src/main/java/com/example/news/scheduler/Scheduler.java
+++ src/main/java/com/example/news/scheduler/Scheduler.java
@@ -2,18 +2,17 @@ package com.example.news.scheduler;
 
 import com.example.news.service.KeywordService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@EnableScheduling
+
 @RequiredArgsConstructor
 @Component
 public class Scheduler {
 
     public final KeywordService keywordService;
 
-    @Scheduled(cron = "* 00 * * * *")
+    @Scheduled(cron = "00 07 * * * *")
     public void scheduled() {
         keywordService.keywordCollectionAll();
     }
